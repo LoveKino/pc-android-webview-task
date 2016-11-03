ef1636b1da201f1a17d906d24f5495742920d42a
loveKino <842914439@qq.com>
2016-11-03 17:37:21 +0800

    fux delete file bug

--- FreekiteAndroidWebViewClient/pc/src/main/java/com/ddchen/bridge/adbpc/MonitorCommand.java
+++ FreekiteAndroidWebViewClient/pc/src/main/java/com/ddchen/bridge/adbpc/MonitorCommand.java
@@ -19,7 +19,7 @@ import java.util.Map;
 public class MonitorCommand {
     private static FileObserver observer = null;
 
-    private static String commandFileName = "command.json";
+    private static String commadFix = "command.json";
 
     // handler queue
     // TODO clear or remove monitor API
@@ -48,11 +48,8 @@ public class MonitorCommand {
             observer = new FileObserver(commandDir) {
                 @Override
                 public void onEvent(int event, String f) {
-                    System.out.println("***********************************");
-                    System.out.println(event);
-                    System.out.println(f);
                     if (f != null &&
-                            f.indexOf(commandFileName) != -1 &&
+                            f.indexOf(commadFix) != -1 &&
                             (event == FileObserver.CLOSE_WRITE || event == FileObserver.MOVED_TO)) {
                         try {
                             final String commandFilePath = commandDir + File.separator + f;
@@ -61,12 +58,12 @@ public class MonitorCommand {
                             byte[] buffer = new byte[(int) commandFile.length()];
                             ins.read(buffer);
                             String command = new String(buffer);
+                            ins.close();
+                            commandFile.delete();
 
                             for (ExecuteCommand handler : finalHandlers) {
                                 handler.execute(command);
                             }
-
-                            ins.close();
                         } catch (FileNotFoundException e) {
                             e.printStackTrace();
                         } catch (IOException e) {
